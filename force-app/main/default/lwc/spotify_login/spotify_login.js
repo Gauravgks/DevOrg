60ed04cb7ce62a1c57edf3a372383ba89e09abf8
Gaurav <cu.16bcs2013@gmail.com>
2024-03-08 06:12:59 +0530

    Spotify Integration Development

---
+++ force-app/main/default/lwc/spotify_login/spotify_login.js
@@ -0,0 +1,28 @@
+import { LightningElement } from 'lwc';
+
+export default class Spotify_login extends LightningElement {
+    // clickedButtonLabel;
+
+    // handleClick(event) {
+    //     //this.clickedButtonLabel = event.target.label;
+    //     var client_id = 'eae8300d36c94138ad8f9d99cb50b1d5';
+    //     var redirect_uri = 'https://gksdevorg-dev-ed.develop.my.salesforce.com/services/authcallback/Spotify';
+
+    //     var app = express();
+
+    //     app.get('/login', function (req, res) {
+
+    //         var state = generateRandomString(16);
+    //         var scope = 'user-read-private user-read-email';
+
+    //         res.redirect('https://accounts.spotify.com/authorize?' +
+    //             querystring.stringify({
+    //                 response_type: 'code',
+    //                 client_id: client_id,
+    //                 scope: scope,
+    //                 redirect_uri: redirect_uri,
+    //                 state: state
+    //             }));
+    //     })
+    // }
+}
