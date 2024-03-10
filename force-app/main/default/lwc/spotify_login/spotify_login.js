c0d9e3b94ea1a34975a001425d8abb8b85c56e5c
Gaurav <cu.16bcs2013@gmail.com>
2024-03-10 10:38:28 +0530

    Spotify user detail added in LWC

--- force-app/main/default/lwc/spotify_login/spotify_login.js
+++ force-app/main/default/lwc/spotify_login/spotify_login.js
@@ -1,28 +1,21 @@
 import { LightningElement } from 'lwc';
+import getUserDetail from '@salesforce/apex/Spotify_HttpCallouts.getUserDetail'
 
 export default class Spotify_login extends LightningElement {
-    // clickedButtonLabel;
 
-    // handleClick(event) {
-    //     //this.clickedButtonLabel = event.target.label;
-    //     var client_id = 'eae8300d36c94138ad8f9d99cb50b1d5';
-    //     var redirect_uri = 'https://gksdevorg-dev-ed.develop.my.salesforce.com/services/authcallback/Spotify';
 
-    //     var app = express();
+    spotifyUser;
+    imageUrl;
+    connectedCallback(){
+        getUserDetail()
+            .then((result) => {
+                let parsedData = JSON.parse(result);
+                this.spotifyUser = parsedData.display_name;
+               this.imageUrl = parsedData.images[0].url;
 
-    //     app.get('/login', function (req, res) {
-
-    //         var state = generateRandomString(16);
-    //         var scope = 'user-read-private user-read-email';
-
-    //         res.redirect('https://accounts.spotify.com/authorize?' +
-    //             querystring.stringify({
-    //                 response_type: 'code',
-    //                 client_id: client_id,
-    //                 scope: scope,
-    //                 redirect_uri: redirect_uri,
-    //                 state: state
-    //             }));
-    //     })
-    // }
+            })
+            .catch((error) => {
+                console.log('##Error '+ JSON.stringify(error));
+            });
+    }
 }
