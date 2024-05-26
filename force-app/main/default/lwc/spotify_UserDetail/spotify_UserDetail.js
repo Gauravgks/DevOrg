4f80c00a9507ddcd8ddc7d14787cc7d505fb66ea
Gaurav <cu.16bcs2013@gmail.com>
2024-05-26 10:56:11 +0530

    spotify project code addition

--- force-app/main/default/lwc/spotify_UserDetail/spotify_UserDetail.js
+++ force-app/main/default/lwc/spotify_UserDetail/spotify_UserDetail.js
@@ -1,7 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import getUserDetail from '@salesforce/apex/Spotify_HttpCallouts.getUserDetail'
 import getDeviceDetails from '@salesforce/apex/Spotify_HttpCallouts.getDeviceDetails'
-
+import getCurrentPlayingSong from '@salesforce/apex/Spotify_HttpCallouts.getCurrentPlayingSong'
 export default class Spotify_login extends LightningElement {
 
     spotifyUser;
@@ -9,8 +9,10 @@ export default class Spotify_login extends LightningElement {
     country;
     Subscription;
     Email;
-    DevicesName;
-    DevicesType
+    DevicesName = []
+    DevicesType = []
+    @track likeState
+    currentSong
     connectedCallback() {
         getUserDetail()
             .then((result) => {
@@ -28,22 +30,40 @@ export default class Spotify_login extends LightningElement {
             });
 
         getDeviceDetails()
-        .then((result)=>{
-            let deviceData = JSON.parse(result);
-            console.log("Log Value ~ Spotify_login ~ .then ~ result:", result)
-            if(result != 'No Device'){
-                this.DevicesName = deviceData.devices[0].name;
-                this.DevicesType = deviceData.devices[0].type;
-            }
-            else{
+            .then((result) => {
+                let deviceData = JSON.parse(result);
+                if (result !== 'No Device') {
+                    let availableDevices = deviceData.devices;
+                    availableDevices.forEach(element => {
+                        this.DevicesName.push(element.name);
+                        this.DevicesType.push(element.type);
 
-                this.DevicesName = "No Active Device";
-                this.DevicesType = "No Active DeviceType ";
-            }
-           
+                    });
+                }
+                else {
+
+                    this.DevicesName = "No Active Device";
+                    this.DevicesType = "No Active DeviceType ";
+                }
+
+            })
+            .catch((error) => {
+                console.log('## Device Error' + error);
+            });
+    }
+    // handleLikeButtonClick() {
+    //     this.likeState = !this.likeState;
+    // }
+
+
+    handleClick() {
+        getCurrentPlayingSong().then((result) => {
+
+            let parsedData = JSON.parse(result);
+
+            this.currentSong = parsedData.item.name;
+            console.log("Log Value ~ Spotify_login ~ .then ~ current song details:", parsedData);
+
         })
-        .catch((error) =>{
-            console.log('## Device Error' + error);
-        });
-}
+    }
 }
